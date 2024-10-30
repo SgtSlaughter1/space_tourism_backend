3e231680a53f3186a2dde6179ff90e4f4ff81909
Abdulsalam <habeebabdulsalam86@gmail.com>
2024-10-30 17:48:25 +0100

    updated the index.js and the main.js file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
 const server = jsonServer.create();
-const router = jsonServer.router('data.json'); // Point to your data file
+const router = jsonServer.router('data.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
-  console.log('JSON Server is running');
-});
+
+export default server;
